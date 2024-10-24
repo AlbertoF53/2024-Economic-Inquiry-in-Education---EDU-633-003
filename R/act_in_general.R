b04db777ce6e24ac9b1d9d3c01b9c70bd5034ccb
AlbertoF53 <cabrera@umd.edu>
2024-10-24 18:18:01 -0400

    Updating figure October 24, 2024

--- R/act_in_general.R
+++ R/act_in_general.R
@@ -30,86 +30,16 @@ library(gfonts)
 library(janitor)
 library(Hmisc)
 library(expss)
+library(ggiraph)
 
-
-# Importing excel files ----------------------------------------------------
-
-library(readxl)
-act_scores_1993_2022 <- read_excel("act-scores_1993_2022.xlsx")
-View(act_scores_1993_2022)
-
-
-      
-title <- "El **covid-19** aceleró la disminución en la preparación para la universidad según"
-subtitle <- "lo evaluado por la prueba de ingreso ACT: 1993-2022"
-caption <- "Fuente: Average ACT Score for 2022, 2021, 2020, 2019, 2018, and Earlier Years"
-
-act_scores_1993_2022 %>% 
-  ggplot(aes(x = year, y = composite)) +
-  geom_line(color = "green", linewidth = 1.5) +
-  geom_point(color = "grey21", shape = 21, fill = "white") +
-  scale_x_continuous(breaks = seq(1993, 2022, 7)) +
-  scale_y_continuous(breaks = seq(19.8,22,0.1)) +
-  labs(title = title,
-       subtitle = subtitle,
-       caption = caption,
-       y = element_blank(), 
-       x = element_blank()) +
-  theme_minimal(
-    base_size = 18,
-    base_family = "Source Sans Pro 3") +
-  theme(
-    plot.title = ggtext::element_markdown(family = "Merriweather", 
-                                          size = 14, margin = margin(b = 0.1, unit = 'cm')),
-    plot.title.position = "plot",
-    plot.subtitle = element_text(family = "Merriweather", size = 14,
-                                 margin = margin(b = 1, unit = 'cm')),
-    plot.caption = element_text(family = "Merriweather", size = 8, hjust = 0),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.line = element_line(color = "black"))
-
-
-#* ACT overall English version
-
-title_eng <- "**COVID-19** acelerated the decline in readiness for college as appraised by"
-subtitle_eng <- "ACT: 1993-2022"
-source_eng <- "Source: Average ACT Score for 2022, 2021, 2020, 2019, 2018, and Earlier Years"
-
-act_scores_1993_2022 %>% 
-  ggplot(aes(x = year, y = composite)) +
-  geom_line(color = "green", linewidth = 1) +
-  geom_point(color = "gray31", shape = 21, fill = "white") +
-  scale_x_continuous(breaks = seq(1993, 2022, 7)) +
-  scale_y_continuous(breaks = seq(19.8,22,0.1)) +
-  labs(title = title_eng,
-       subtitle = subtitle_eng,
-       caption = source_eng ,
-       y = element_blank(), 
-       x = element_blank()) +
-  theme_minimal(
-    base_size = 18,
-    base_family = "Source Sans Pro 3") +
-  theme(
-    plot.title = ggtext::element_markdown(family = "Merriweather", 
-                                          size = 14, margin = margin(b = 0.1, unit = 'cm')),
-    plot.title.position = "plot",
-    plot.subtitle = element_text(family = "Merriweather", size = 14,
-                                 margin = margin(b = 1.0, unit = 'cm')),
-    plot.caption = element_text(family = "Merriweather", size = 8, hjust = 0),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.line = element_line(color = "black"))
-
+#ACT Interactive
 
 
 library(readxl)
-act_ethnic_english <- read_excel("act_ethnic_english.xlsx")
-View(act_ethnic_english)
+act_populations_english <- read_excel("data/act_populations_english.xlsx")
+View(act_populations_english)
 
-act_disparities <-  act_ethnic_english %>% 
+act_disparities <-  act_populations_english %>% 
   pivot_longer(
     cols = 2:11,
     names_to = "year",
@@ -118,19 +48,21 @@ act_disparities <-  act_ethnic_english %>%
 
 
 
-title_disp <- "Pronounced differences in readiness for college (ACT) across different "
-subtitle_disp <- "populations: 2001 - 2022"
+title_disp <- "Pronounced differences in readiness for college (**ACT**) across"
+subtitle_disp <- "different populations: 2001 - 2022"
 caption <- "Source: Average ACT Score for 2022, 2021, 2020, 2019, 2018, and Earlier Years"
 
 
-act_disparities %>% 
+act_inter <- act_disparities %>% 
   na.omit() %>% 
   filter(ethnicity %in% c('Native American','African American', 'Asian American',
-                      'White', 'Latinx', 'Multiple')) %>%
+                          'European', 'Latinx', 'Multiple')) %>%
+  mutate(
+    tooltip_label = glue::glue('Mean ACT in {year}<br>was {round(act,2)}')) %>% 
   ggplot(aes(x = year, y = act, group = ethnicity, color = ethnicity, linetype = ethnicity)) +
-  geom_point() +
-  geom_line(linewidth = 1) +
-  scale_y_continuous(breaks = seq(16,27)) +
+  geom_point_interactive(aes(tooltip = tooltip_label, data_id = ethnicity), size = 3.5) +
+  geom_line_interactive(aes(data_id = ethnicity), linewidth = 2.5) +
+  scale_y_continuous(breaks = seq(10,27)) +
   labs(title = title_disp,
        subtitle = subtitle_disp,
        caption = caption,
@@ -139,104 +71,39 @@ act_disparities %>%
   scale_color_brewer(palette = "Dark2") +
   theme_minimal(
     base_size = 18,
-    base_family = "Source Sans Pro 3") +
+    base_family = "Source Sans Pro") +
   theme(
     legend.title = element_blank(),
-    plot.title = ggtext::element_markdown(family = "Merriweather", size = 14),
-    plot.subtitle = element_text(family = "Merriweather", size = 14),
+    legend.text = element_text(size = 10),
+    plot.title = ggtext::element_markdown(family = "Merriweather", size = 18, margin = margin(b = 0.1, unit = 'cm')),
+    plot.subtitle = element_text(family = "Merriweather", size = 18, margin = margin(b = 1, unit = 'cm')),
     plot.title.position = "plot",
     plot.caption = element_text(family = "Merriweather", size = 8, hjust = 0),
     panel.grid.minor.y = element_blank(),
     panel.grid.major = element_blank(),
     panel.grid.minor.x = element_blank(),
     axis.line = element_line(color = "black"),
-    legend.position = "bottom") 
-
-#* Spanish version
-
-library(readxl)
-disparities_ACT <- read_excel("disparities_ACT.xlsx")
-View(disparities_ACT )
+    legend.position = "bottom")   
 
 
-act_disparities <-  disparities_ACT  %>% 
-  pivot_longer(
-    cols = 2:11,
-    names_to = "year",
-    values_to = "act"
-  )
-
-
-
-
-
-
-act_disparities %>% 
-  na.omit() %>% 
-  filter(etnia %in% c('Aborigen','Africano', 'Asiatico',
-                      'Europeo', 'Latinoamericano','Multiple')) %>%
-  ggplot(aes(x = year, y = act, group = etnia, color = etnia, linetype = etnia)) +
-  geom_point() +
-  geom_line(linewidth = 1) +
-  scale_y_continuous(breaks = seq(16,27)) +
-  labs(title = title_disp,
-       subtitle = subtitle_disp,
-       caption = caption,
-       y = element_blank(), 
-       x = element_blank()) +
-  scale_color_brewer(palette = "Dark2") +
-  theme_minimal() +
-  theme(
-    legend.title = element_blank(),
-    plot.title = ggtext::element_markdown(family = "Merriweather", size = 14),
-    plot.subtitle = element_text(family = "Merriweather", size = 14),
-    plot.title.position = "plot",
-    plot.caption = element_text(family = "Merriweather", size = 8, hjust = 0),
-    panel.grid.minor.y = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    axis.line = element_line(color = "black"),
-    legend.position = "bottom") 
-
-
-# Wanting and enrolling in college ----------------------------------------
-
-
-library(readxl)
-planning_to_attend <- read_excel("planning-to_attend.xlsx") %>% 
-  clean_names()
-
-View(planning_to_attend)
-
-wanting <-  planning_to_attend %>% 
-  pivot_longer(
-    cols = 3:4,
-    names_to = "matricularse",
-    values_to = "porcentage"
-  )
-
-caption <-  "Fuente: Carnevale et al., (2019), p. 12"
-
-  
-  
-wanting %>% 
-  ggplot(aes(x = fct_reorder(ingreso_familiar,order), y = porcentage, fill = matricularse)) +
-  geom_col(position = "dodge") +
-  geom_text(aes(y = porcentage, label = porcentage),
-            vjust = 0.08, 
-            color = "black", position = position_dodge(0.9)) +
-  labs(x = element_blank(),
-       fill = "Matricularse:",
-       caption = caption) + 
-  theme_bw() +
-  theme(
-    panel.grid.minor.y = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.major.x = element_blank(),
-    axis.line = element_line(color = "black"),
-    legend.position = "top")
-  
-
+girafe(
+  ggobj = act_inter,
+  width_svg = 8,
+  options = list(
+    opts_hover(css = ''),
+    opts_hover_inv(
+      css = girafe_css(
+        css = 'opacity:0.4;',
+        line = 'opacity:0.075;'
+      )
+    ),
+    opts_tooltip(
+      css = htmltools::css(
+        background = 'white',
+        border = '2px solid black;',
+        padding = '10px',
+        font_weight = 600,
+        font_size = '14p'))))
 
 
 
